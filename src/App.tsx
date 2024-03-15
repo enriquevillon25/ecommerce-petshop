e1b92d226b15ee216d2cc636921c9d1badbdee0b
Enrique Villón <enriquevillon2597@gmail.com>
2024-03-15 00:23:57 -0500

    feature/add-react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { HeaderComponent } from "./components/header/HeaderComponent";
 import { HomePage } from "./pages/home/HomePage";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { ProductsPage } from "./pages/products/ProductsPage";
 
 function App() {
   return (
-    <Fragment>
+    <BrowserRouter>
       <HeaderComponent />
-      <HomePage />
-    </Fragment>
+      <Routes>
+        <Route Component={HomePage} path="" />
+        <Route Component={ProductsPage} path="/products" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
